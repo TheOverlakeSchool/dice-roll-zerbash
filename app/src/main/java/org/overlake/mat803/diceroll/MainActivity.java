f90fb61256627f1bef01da930a1fce2dc880241a
zerbash <dbasham@overlake.org>
2022-01-21 06:55:17 -0800

    1-19-22

--- app/src/main/java/org/overlake/mat803/diceroll/MainActivity.java
+++ app/src/main/java/org/overlake/mat803/diceroll/MainActivity.java
@@ -3,6 +3,7 @@ package org.overlake.mat803.diceroll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NumberPicker picker = findViewById(R.id.picker);
+        picker.setMinValue(1);
+        picker.setMaxValue(6);
+        picker.setWrapSelectorWheel(false);
     }
 }
