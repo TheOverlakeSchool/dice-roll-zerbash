95ae78560bc977a3f47c837f9d86e53f103bfc1f
zerbash <dbasham@overlake.org>
2022-01-27 09:17:29 -0800

    1-27-33

--- app/src/main/java/org/overlake/mat803/diceroll/Agreement.java
+++ app/src/main/java/org/overlake/mat803/diceroll/Agreement.java
@@ -2,7 +2,9 @@ package org.overlake.mat803.diceroll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Agreement extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Agreement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+        findViewById(R.id.agree).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Agreement.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
